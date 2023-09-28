b8d3f6e2a44e0b940d54845eb94bbfda6782b0a2
Randell157 <Randelljustinlapid@gmail.com>
2023-09-28 02:47:27 -0700

    js updating

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const display = document.querySelector('.inputBar')
-const buttons = document.querySelectorAll('button')
+const display = document.querySelector('inputBar')
+const numpad = document.querySelectorAll('button')
 
-function calculator{
-    
-}
+numpad.forEach(button => button.addEventListener('click', ))
