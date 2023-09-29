4819b1adee352acf8b09cc080595c1c475d29018
Randell157 <Randelljustinlapid@gmail.com>
2023-09-28 22:56:49 -0700

    Added input to screen

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const display = document.querySelector('inputBar')
-const numpad = document.querySelectorAll('button')
+const numpad = document.querySelectorAll('button');
+const display = document.querySelector('input');
+
+for (let button of numpad) {
+    button.addEventListener("click", calculator);
+}
+
+function calculator(event) {
+    let value = event.target.innerHTML;
+    display.value += value;
+
+    console.log(value);
+}
