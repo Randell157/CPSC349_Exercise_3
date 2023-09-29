868d5a8718d3680bbc29dd8ca62ff223d7e3b5c7
Randell157 <Randelljustinlapid@gmail.com>
2023-09-29 15:46:24 -0700

    Updated script functional calculator

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
-const numpad = document.querySelectorAll('button');
-const display = document.querySelector('input');
+const numpad = document.querySelectorAll("button");
+const display = document.querySelector("input");
 
 for (let button of numpad) {
-    button.addEventListener("click", calculator);
+  button.addEventListener("click", calculator);
 }
 
 function calculator(event) {
-    let value = event.target.innerHTML;
-    if(value === 'C'){
-        display.value = '';
+  let value = event.target.innerHTML;
+  if (value === "C") {
+    display.value = "";
+  } else if (value == "=") {
+    let result = display.value;
+    display.value = eval(result);
+  } else if (value == "+" || value == "-" || value == "*" || value == "/") {
+    if (display.value == "") {
+      alert("Empty Calculator");
     }
-    else if(value == '='){
-        let result = display.value;
-        display.value = eval(result);
+    if (display.value.includes("+")) {
+      display.value = display.value.replace("+", value);
+    } else if (display.value.includes("-")) {
+      display.value = display.value.replace("-", value);
+    } else if (display.value.includes("*")) {
+      display.value = display.value.replace("*", value);
+    } else if (display.value.includes("/")) {
+      display.value = display.value.replace("/", value);
+    } else {
+      display.value += value;
     }
-    else if(value == '+' || value == '-' ||value == '*'||value == '/'){
-        if (display.value == ''){
-            alert("Empty Calculator");
-        }
-        if(display.value.includes('+')){
-            display.value = display.value.replace('+', value);
-        }
-        if(display.value.includes('-')){
-            display.value = display.value.replace('-', value);
-        }
-        if(display.value.includes('*')){
-            display.value = display.value.replace('*', value);
-        }
-        if(display.value.includes('/')){
-            display.value = display.value.replace('/', value);
-        }
-    }
-    else{
-        display.value += value;
-    }
-    console.log(display.value);
+  } else {
+    display.value += value;
+  }
+  console.log(display.value);
 }
